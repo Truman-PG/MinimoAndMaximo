c0b3ff83f2c36bd6304b8eef41d4d489f3bb99ec
Aluno <Aluno@DESKTOP-DMGHJ3C>
2024-04-27 12:18:46 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numeros = new int[10];
+int minimo, maximo;
+
+
+for (int indice = 0; indice < numeros.Length; indice++)
+{
+    Console.WriteLine($"Qual o numero {indice+1}?");
+    int numero = int.Parse(Console.ReadLine());
+    numeros[indice] = numero;
+   
+}
+minimo = numeros[0];
+maximo = numeros[0];
+
+for(int indice = 0; indice < numeros.Length; indice++)
+{
+   if (maximo < numeros[indice])
+    
+        maximo = numeros[indice];
+
+    if (minimo > numeros[indice])
+
+        minimo = numeros[indice];
+}
+Console.WriteLine($"Mínimo: {minimo}");
+Console.WriteLine($"Máximo {maximo}");
